085c4696ba6bc2422bebb280faaac463ed49af59
iwanf <firmawaneiwan@gmail.com>
2019-07-10 19:26:57 +0700

    uncomment dispatch payment reminder

--- modules/preorder/src/Http/Controllers/Api/TransactionApiController.php
+++ modules/preorder/src/Http/Controllers/Api/TransactionApiController.php
@@ -324,7 +324,7 @@ class TransactionApiController extends Controller
                 $interval   = $interval * 60;
                 for ($i=1; $i <= $repeat; $i++) { 
                     $interval = $i * $interval;
-                    //dispatch(new PaymentReminder($transaction))->delay($transaction->created_at->addMinutes($interval));
+                    dispatch(new PaymentReminder($transaction))->delay($transaction->created_at->addMinutes($interval));
                 }
                 
             } catch (Exception $ex) {
